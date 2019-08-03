80c3930c3b14798a46da7bca8512a34ce51ac04a
satyam <39213202+krsatyam20@users.noreply.github.com>
2019-08-03 20:31:32 +0530

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,28 @@
+
+function add(){
+
+
+//alert("hello");
+//document.write("hello")
+//console.log("hello");
+var  x= document.getElementById("num1").value;
+var  y= document.getElementById("num2").value;
+
+x= parseInt(x);
+y= parseInt(y);
+console.log(typeof(x));
+console.log(typeof(y));
+var z= x+ y ;
+
+document.getElementById("res").value=z;
+
+}
+
+
+function hide(){
+	
+	
+	
+	
+}
+
